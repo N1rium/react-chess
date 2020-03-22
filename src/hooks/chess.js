ee504811c44420a97c9e68ef5277598d3a765fd1
blomman <johnny@gumbler.com>
2020-03-22 02:16:52 +0100

    Fixed chess return error

--- src/hooks/chess.js
+++ src/hooks/chess.js
@@ -5,7 +5,7 @@ import Chess from 'chess.js';
 const useChess = () => {
   const [chess, setChess] = useState(new Chess());
   useEffect(() => {}, []);
-  return chess;
+  return { chess };
 };
 
 export default useChess;
