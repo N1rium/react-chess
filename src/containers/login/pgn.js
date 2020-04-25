b24fb2f378456f221633bb236f2f07dc44d34467
blomman <johnny@gumbler.com>
2020-04-25 15:58:23 +0200

    Added PGNs

---
+++ src/containers/login/pgn.js
@@ -0,0 +1,65 @@
+export const VincentVsMagnus = `[Event "GRENKE Chess Classic 2019"]
+[Site "Karlsruhe/Baden Baden GER"]
+[Date "2019.04.20"]
+[Round "1.1"]
+[White "Keymer,Vincent"]
+[Black "Carlsen,M"]
+[Result "0-1"]
+[WhiteTitle "IM"]
+[BlackTitle "GM"]
+[WhiteElo "2516"]
+[BlackElo "2845"]
+[ECO "A56"]
+[Opening "Benoni defence"]
+[WhiteFideId "12940690"]
+[BlackFideId "1503014"]
+[EventDate "2019.04.20"]
+
+1. d4 Nf6 2. c4 c5 3. d5 g6 4. Nc3 d6 5. e4 Bg7 6. Nf3 O-O 7. Be2 e5 8. O-O Ne8
+9. Ne1 f5 10. exf5 gxf5 11. f4 Nd7 12. Nd3 e4 13. Nf2 Bxc3 14. bxc3 Ndf6 15. Be3
+Ng7 16. Qe1 Bd7 17. Nd1 Ba4 18. h3 Bxd1 19. Qxd1 Qe8 20. Kf2 Qg6 21. Rg1 Kh8 22.
+a4 Rg8 23. Qf1 Nfh5 24. g3 Raf8 25. Qg2 Qf6 26. Rac1 Qd8 27. Qh2 Nf6 28. g4 Nd7
+29. g5 Qa5 30. g6 h6 31. Rb1 Rb8 32. Qg3 Qd8 33. Ke1 Ne8 34. Kd2 Nf8 35. Bf2 Qe7
+36. Ke3 Qf6 37. Kd2 Nxg6 38. h4 Ne7 39. Qh3 Rxg1 40. Rxg1 Qf7 41. h5 Nf6 42. Bh4
+b6 43. Rb1 Qf8 44. Rg1 Qf7 45. Rb1 Qg7 46. Rg1 Qf8 47. Kc2 Nfg8 48. Kd2 Qf7 49.
+Kc2 Rf8 50. Kd2 Qe8 51. Ra1 Rf7 52. a5 bxa5 53. Rxa5 Nc8 54. Ra1 Qf8 55. Rb1 Nb6
+56. Rg1 Rg7 57. Rxg7 Kxg7 58. Qg3+ Kh8 59. Qg6 a5 60. Bf1 a4 61. Kc2 a3 62. Kb3
+Na4 63. Bh3 Qg7 64. Qxg7+ Kxg7 65. Bxf5 Nf6 66. Kxa3 Nxc3 67. Bf2 Ne2 68. Ka4
+Nxh5 69. Ka5 Nf6 70. Kb6 Kf7 71. Kc7 Ke7 72. Be3 Nd4 73. Bg6 h5 74. Bf2 Nf3 75.
+Bf5 Nd2 76. Bh4 e3 77. Bd3 Nf3 78. Bxf6+ Kxf6 79. Kxd6 h4 80. Kc7 Nd4 81. Kc8 e2 0-1`;
+
+export const UweVsDrazen = `[Event "FRG-ch International"]
+[Site "Dortmund"]
+[Date "1973.??.??"]
+[Round "15"]
+[White "Kunsztowicz, Uwe"]
+[Black "Marovic, Drazen"]
+[Result "0-1"]
+[WhiteElo ""]
+[BlackElo "2465"]
+[ECO "D18"]
+
+1.d4 d5 2.c4 c6 3.Nf3 Nf6 4.Nc3 dxc4 5.a4 Bf5 6.e3 e6 7.Bxc4 Nbd7 8.O-O Bb4
+9.Qe2 Bg6 10.Na2 Bd6 11.b3 O-O 12.Bb2 Qb8 13.h3 Re8 14.Nc3 e5 15.Rad1 exd4
+16.Rxd4 Ne5 17.Rfd1 Bc5 18.R4d2 Bh5 19.g4 Nexg4 20.hxg4 Nxg4 21.Rd4 Bxd4
+22.Rxd4 Ne5 23.Rf4 Nxf3+ 24.Rxf3 Re5 25.Kf1 Qd8 26.Ke1 Rg5 27.Kf1 Qd7 28.Ne4 Rf5
+29.Rg3 Bxe2+ 30.Kxe2 g6 31.Nf6+ Rxf6 32.Bxf6 b5 33.Bd3 Qe6 34.Bd4 Qxb3 35.axb5 cxb5
+36.Rg5 a6 37.Rc5 Rd8 38.Be4 Qa2+ 39.Kf3 Qe6  0-1`;
+
+export const SpasskyVsFischer = `[Event "World Championship 28th"]
+[Site "Reykjavik"]
+[Date "1972.??.??"]
+[Round "21"]
+[White "Spassky, Boris V"]
+[Black "Fischer, Robert James"]
+[Result "0-1"]
+[WhiteElo "2660"]
+[BlackElo "2785"]
+[ECO "B46"]
+
+1.e4 c5 2.Nf3 e6 3.d4 cxd4 4.Nxd4 a6 5.Nc3 Nc6 6.Be3 Nf6 7.Bd3 d5 8.exd5 exd5
+9.O-O Bd6 10.Nxc6 bxc6 11.Bd4 O-O 12.Qf3 Be6 13.Rfe1 c5 14.Bxf6 Qxf6 15.Qxf6 gxf6
+16.Rad1 Rfd8 17.Be2 Rab8 18.b3 c4 19.Nxd5 Bxd5 20.Rxd5 Bxh2+ 21.Kxh2 Rxd5
+22.Bxc4 Rd2 23.Bxa6 Rxc2 24.Re2 Rxe2 25.Bxe2 Rd8 26.a4 Rd2 27.Bc4 Ra2 28.Kg3 Kf8
+29.Kf3 Ke7 30.g4 f5 31.gxf5 f6 32.Bg8 h6 33.Kg3 Kd6 34.Kf3 Ra1 35.Kg2 Ke5
+36.Be6 Kf4 37.Bd7 Rb1 38.Be6 Rb2 39.Bc4 Ra2 40.Be6 h5 41.Bd7  0-1`;
