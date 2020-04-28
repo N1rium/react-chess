a15272f4df0d34142c0e2b08af9c7c8841d8e40e
blomman <johnny@gumbler.com>
2020-04-28 10:45:10 +0200

    Added ELO utility functions to chess-helper

--- src/utils/chess-helper.js
+++ src/utils/chess-helper.js
@@ -126,3 +126,21 @@ export const fenToObj = (fen) => {
 
   return position;
 };
+
+/* ================================================ ELO ========================================================== /*
+
+/* Get expected ELO */
+export const expectedELO = (a, b) => {
+  return 1 / (1 + Math.pow(10, (b - a) / 400));
+};
+
+/* Get updated ELO from expected */
+export const updateELO = (expected, result, current, k = 20) => {
+  return Math.round(current + k * (result - expected));
+};
+
+/* Get expected ELO and update it */
+export const eloChange = (a, b, res, k) => {
+  const expected = expectedELO(a, b);
+  return updateELO(expected, res, a);
+};
