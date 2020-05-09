8f6e145152a4a31b43089c5aa421a7c49d0cf6da
blomman <johnny@gumbler.com>
2020-05-09 02:01:29 +0200

    Added global chat

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSubscription, useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Chat from '../../components/chat';
 import Profile from './components/profile';
@@ -6,6 +8,24 @@ import Play from './components/play';
 import Ongoing from './components/ongoing';
 import Finished from './components/finished';
 import Matchmaking from './components/matchmaking';
+
+export const SEND_CHAT_MESSAGE = gql`
+  mutation sendChatMessage($input: ChatMessageInput!) {
+    sendChatMessage(input: $input) {
+      sender
+      content
+    }
+  }
+`;
+
+export const CHAT_SUBSCRIPTION = gql`
+  subscription chatMessage($room: String!) {
+    chatMessage(room: $room) {
+      sender
+      content
+    }
+  }
+`;
 
 const Layout = styled.div`
   display: grid;
@@ -31,6 +51,16 @@ const ChatContainer = styled.section`
 `;
 
 export default () => {
+  const [chatMessages, setChatMessages] = useState([]);
+  const { data: chatData } = useSubscription(CHAT_SUBSCRIPTION, { variables: { room: 'global' } });
+  const [sendChatMessage] = useMutation(SEND_CHAT_MESSAGE);
+
+  useEffect(() => {
+    if (chatData && chatData.chatMessage) {
+      setChatMessages([...chatMessages, chatData.chatMessage]);
+    }
+  }, [chatData]);
+
   return (
     <Layout>
       <Profile />
@@ -38,7 +68,10 @@ export default () => {
       <Matchmaking />
       <ChatContainer>
         <header>Chat</header>
-        <Chat />
+        <Chat
+          messages={chatMessages}
+          onSendMessage={(message) => sendChatMessage({ variables: { input: { room: 'global', message } } })}
+        />
       </ChatContainer>
       <Ongoing />
       <Finished />
